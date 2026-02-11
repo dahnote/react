21995a8beb518d8fc1c9fe072dbb8b695c1eca41
iServCloudTeam <122348650+iServCloudTeam@users.noreply.github.com>
2026-02-11 16:38:28 +0800

    w1

--- work/01/re/src/App.jsx
+++ work/01/re/src/App.jsx
@@ -2,46 +2,114 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [apiData, setApiData] = useState([
+    {
+      title: '綠色馬卡龍',
+      price: 120,
+      amount: 3,
+    },
+    {
+      title: '綠色馬卡龍2',
+      price: 1202,
+      amount: 23,
+    },
+    {
+      title: '綠色馬卡龍3',
+      price: 1203,
+      amount: 33,
+    },
+    {
+      title: '綠色馬卡龍4',
+      price: 1204,
+      amount: 34,
+    },
+  ]);
+  const [state, setState] = useState(1);
+  const [tempData, setTempData] = useState({});
+  function AddState(num) {
+    setState(state + num);
+  }
+  function removeItem(title) {
+    const index = apiData.findIndex((item) => {
+      return item.title === title;
+    });
+    console.log(index);
+    // setData(data.splice(index, 1));
+    let _data = [...apiData];
+    _data.splice(index, 1);
+    console.log(_data);
+    // data.splice(index, 1);
+    // setData(data);
+    setApiData(_data);
+  }
   return (
     <>
-      <table>
-        <thead>
-          <tr>
-            <th width="200">品名</th>
-            <th>單價</th>
-            <th>數量</th>
-            <th width="150">小計</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>綠色馬卡龍</td>
-            <td>120</td>
-            <td>3</td>
-            <td>$ 120</td>
-          </tr>
-          <tr>
-            <td>粉係馬卡龍</td>
-            <td>120</td>
-            <td>2</td>
-            <td>$ 240</td>
-          </tr>
-          <tr>
-            <td>甜蜜左擁右抱</td>
-            <td>200</td>
-            <td>7</td>
-            <td>$ 1400</td>
-          </tr>
-        </tbody>
-        <tfoot>
-          <tr>
-            <td colspan="3">總計：</td>
-            <td>NT$ 1760</td>
-          </tr>
-        </tfoot>
-      </table>
+      <div>
+        {state}
+        <table>
+          <thead>
+            <tr>
+              <th width="200">品名</th>
+              <th>單價</th>
+              <th>數量</th>
+              <th width="150">小計</th>
+            </tr>
+          </thead>
+          <tbody>
+            {apiData.map((item, index) => {
+              return (
+                <tr key={index}>
+                  <td>{item.title}</td>
+                  <td>{item.price}</td>
+                  <td>{item.amount}</td>
+                  <td>
+                    {item.price * item.amount}
+                    <button
+                      type="button"
+                      onClick={() => {
+                        setTempData(item);
+                      }}
+                    >
+                      選
+                    </button>
+                    <button
+                      type="button"
+                      onClick={() => {
+                        removeItem(item.title);
+                      }}
+                    >
+                      刪除
+                    </button>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+          <tfoot>
+            <tr>
+              <td colSpan="3">總計：</td>
+              <td>NT$ 1760</td>
+            </tr>
+          </tfoot>
+        </table>
+        <button
+          type="button"
+          onClick={() => {
+            setState(state + 1);
+          }}
+        >
+          編輯+1
+        </button>
+        <button
+          type="button"
+          onClick={() => {
+            AddState(2);
+          }}
+        >
+          編輯+2
+        </button>
+        <div>{tempData.title}</div>
+      </div>
     </>
   );
 }
