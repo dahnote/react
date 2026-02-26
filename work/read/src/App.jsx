b12d6ecdadb3ed697621a8ecdd95831aa9fa121a
iServCloudTeam <122348650+iServCloudTeam@users.noreply.github.com>
2026-02-26 17:56:26 +0800

    useState array 操作

--- work/read/src/App.jsx
+++ work/read/src/App.jsx
@@ -2,29 +2,35 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  let [inputValue, setinputValue] = useState({ name: '' });
+  let [arrayValue, setarrayValue] = useState([1, 2, 3]);
   const handleChange = (e) => {
     // console.log(e.target.value);
     // let value = inputValue;
-    const { name, value } = e.target;
-
-    setinputValue({ ...inputValue, [name]: value });
+    // const { name, value } = e.target;
+    // setarrayValue({ ...inputValue, [name]: value });
+    // setarrayValue
   };
   return (
     <>
-      {/* <div>{num}</div>
-      <button type="button" onClick={() => handleClick()}>
-        ++
-      </button> */}
-      <div>{JSON.stringify(inputValue)}</div>
-      <input
+      {
+        <button type="button" onClick={() => handleChange()}>
+          pop
+        </button>
+      }
+      <div>{arrayValue}</div>
+      <ul>
+        {arrayValue.map((i) => {
+          return <li key={i}>{i}</li>;
+        })}
+      </ul>
+      {/* <input
         type="text"
         name="name"
         value={inputValue.name}
         onChange={(e) => {
           handleChange(e);
         }}
-      />
+      /> */}
     </>
   );
 }
