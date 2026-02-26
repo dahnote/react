d793b0e04f3e7bfc37dcb42b1bd1fbfa408f8786
iServCloudTeam <122348650+iServCloudTeam@users.noreply.github.com>
2026-02-26 09:18:17 +0800

    調整

--- work/w02/re/vite-project/vite.config.js
+++ work/w02/re/vite-project/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/react/work/w02/re/vite-project/',
+    base: '/react/work/w02/re/vite-project/dist/',
     plugins: [react()],
 });
